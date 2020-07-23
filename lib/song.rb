c33898b1bb354be4343b01a247d04d9062bdeba0
Adam Palmer <theadampalmer@gmail.com>
2020-07-23 22:04:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Song
       extend Memorable::ClassMethods
       include Memorable::InstanceMethods
@@ -12,11 +10,6 @@ class Song
   @@songs = []
 
   def initialize
-  #   @@songs << self
-  # end
-
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
     super
   end
 
@@ -24,19 +17,8 @@ class Song
     @@songs
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  # end
-
   def artist=(artist)
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
